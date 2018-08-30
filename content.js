39b46d3c4b2031adcdf7b514337de13e79587412
Nandu kmg <nandukrishnan55555@gmail.com>
2018-08-30 17:39:18 +0530

    add js mutations for title changes

--- content.js
+++ content.js
@@ -1,16 +1,53 @@
-// alert("Hello from your Chrome extension!");
+    // link.type = 'image/x-icon';
+    // link.rel = 'shortcut icon';
+    // link.href = null;
+
+// select the target node
+var target = document.querySelector('title');
+var link = document.querySelector("link[rel='shortcut icon']") || document.createElement('link');
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+    // We need only first event and only new value of the title
+    console.log("yes3");
+    console.log(mutations[0].target.nodeValue);
+    mutations[0].target.textContent="WhatsApp";
+    
+});
+
+var linkObserver = new MutationObserver(function(mutations) {
+    //mutation removes unread chats number from title. totally uneccessary
+    console.log("yes 4");
+    console.log(mutations[0].target.href);
+    if(mutations[0].target.href!="https://web.whatsapp.com/favicon.ico"){
+        // mutations[0].target.href = null;
+        console.log("hell");
+         mutations[0].target.href='favicon.ico';
+        // self loop gaaawd
+    }
+    ;
+    
+});
+
+
+
+// configuration of the observer:
+var config = { subtree: true, characterData: true, childList:true };
+
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
+linkObserver.observe(link,{attributes: true,childList:true});
+
+    // alert("Hello from your Chrome extension!");
 // document.body.style.border = "5px solid red";
-(function() {
-    setTimeout(function(){
-    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-    link.type = 'image/x-icon';
-    link.rel = 'shortcut icon';
-    link.href = 'https://www.stackoverflow.com/favicon.ico';
-    document.getElementsByTagName('head')[0].appendChild(link);
-    document.title = "WhatsApp";
-    console.log("yes");
-    },7000);
-})();
+// (function() {
+//     setTimeout(function(){
+    
+//     // document.getElementsByTagName('head')[0].appendChild(link);
+//     document.title = "WhatsApp";
+//     console.log("yes");
+//     },7000);
+// })();
+
 
 (function(){
     setTimeout(function(){
@@ -19,7 +56,7 @@
         for (i = 0; i < bubbles.length; i++) {
             bubbles[i].style.backgroundColor = "#bebebe";
         }
-        cosnole.log("yes2");
+        // cosnole.log("yes2");
     },10000);
 })();
 (function(){
