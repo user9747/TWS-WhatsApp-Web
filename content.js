86c01e2de88be2c24922987d7bbcd3513478de18
albin <albinpkr96@gmail.com>
2018-09-02 12:09:24 +0530

    observer performance on firefox -fix

--- content.js
+++ content.js
@@ -19,7 +19,7 @@ var observer = new MutationObserver(function(mutations) {
         landingDiv.childNodes[0].innerHTML = "To disconnect is to live ";
         landingDiv.childNodes[1].innerHTML="WhatsApp is addictive. <a href=\"https://humanetech.com/problem/\" target=\"_blank\">Learn more here</a>."
     }
-
+    observer.disconnect()
     
 });
 
@@ -27,67 +27,49 @@ var linkObserver = new MutationObserver(function(mutations) {
     //mutation removes unread chats number from title. totally uneccessary
     console.log("yes 4");
     console.log(mutations[0].target.href);
-    if(mutations[0].target.href!="https://web.whatsapp.com/favicon.ico"){
+    if(mutations[0].target.href!="favicon.ico"){
         // mutations[0].target.href = null;
         console.log("hell");
          mutations[0].target.href='favicon.ico';
+         console.log(mutations[0].target.href);
+         console.log('hello')
         // self loop gaaawd
-    }
-    ;
+    };
+    linkObserver.disconnect();
+    
     
 });
 
 
 
 
+
+
 // configuration of the observer:
-var config = { subtree: true, characterData: true, childList:true };
+var config = {characterData: true, childList:true };
 
-// pass in the target node, as well as the observer options
-observer.observe(target, config);
-linkObserver.observe(link,{attributes: true,childList:true});
-
-    // alert("Hello from your Chrome extension!");
-// document.body.style.border = "5px solid red";
-// (function() {
-//     setTimeout(function(){
-    
-//     // document.getElementsByTagName('head')[0].appendChild(link);
-//     document.title = "WhatsApp";
-//     console.log("yes");
-//     },7000);
-// })();
+//-----OPTION 1-----//(feels better performnce on this one)
 
 
-(function(){
-    setTimeout(function(){
-        var bubbles = document.querySelectorAll(".OUeyt");
-        var i;
-        for (i = 0; i < bubbles.length; i++) {
-            bubbles[i].style.backgroundColor = "#bebebe";
-        }
-        // cosnole.log("yes2");
-    },10000);
-})();
-// (function(){
-//     setTimeout(function(){
-//         var dialogue = document.querySelectorAll(".iHhHL");
-//         var i;
-//         for (i = 0; i < dialogue.length; i++) {
-//             dialogue[i].innerHTML = "To disconnect is to live ";
-//         }
-//         console.log("yes2");
-//     },5000);
-// })();
-// (function(){
-//     setTimeout(function(){
-//         var dialogue = document.querySelectorAll("._1lrYJ");
-//         var i;
-//         for (i = 0; i < dialogue.length; i++) {
-//             dialogue[i].innerHTML = "WhatsApp is addictive. <a href=\"https://humanetech.com/problem/\" target=\"_blank\">Learn more here</a>.";
-//         }
-//         console.log("yes2");
-//     },10000);
-// })();
+// test = function(){
+// setTimeout(function(){
+//     observer.observe(target, config);
+//     link.href='favicon.ico';
+//     linkObserver.observe(link,config);
+//     console.log("yes2");
+//     test();
+// },2000);
+// }
+// test();
 
+
+//------OPTION 2------//
+
+
+setInterval(function(){ 
+    observer.observe(target, config);
+    link.href='favicon.ico';
+    linkObserver.observe(link,config);
+    console.log("yes2");
+ },2000);
 
